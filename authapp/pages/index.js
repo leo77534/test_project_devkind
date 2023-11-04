9a2d44ee076b549d4adb608e5fdd45618270f311
leo77534 <zhaofag@student.unimelb.edu.au>
2023-11-05 01:29:43 +1100

    Feat: add home page for guest and user

--- authapp/pages/index.js
+++ authapp/pages/index.js
@@ -2,18 +2,64 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Link from "next/link";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [session, setSession] = useState(true);
+
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <title>Home Page</title>
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      </main>
-    </>
+      {session ? User() : Guest()}
+    </div>
+  );
+}
+
+// Guset
+function Guest() {
+  return (
+    <main className="container mx-auto text-center py-20">
+      <h3 className="text-4xl font-bold">Guest Homepage</h3>
+      <div className="flex justify-center">
+        <Link href={"/login"} legacyBehavior>
+          <a className="mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-gray-50">
+            Sign In
+          </a>
+        </Link>
+      </div>
+    </main>
+  );
+}
+
+// Authorise user
+function User() {
+  return (
+    <main className="container mx-auto text-center py-20">
+      <h3 className="text-4xl font-bold">User Homepage</h3>
+
+      <div className="detail">
+        <h5>Unknown</h5>
+        <h5>Unknown</h5>
+      </div>
+
+      <div className="flex justify-center">
+        <button className="mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-gray-50">
+          Sign Out
+        </button>
+      </div>
+
+      <div className="flex justify-center">
+        <Link href={"/profile"} legacyBehavior>
+          <a className="mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-gray-50">
+            Profile Page
+          </a>
+        </Link>
+      </div>
+    </main>
   );
 }

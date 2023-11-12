4f7dd3abf00e8be4c9ffd191b6d1dd441bce7866
leo77534 <68187502+leo77534@users.noreply.github.com>
2023-11-12 20:26:30 +1100

    Docs: add comments

--- authapp/pages/_app.js
+++ authapp/pages/_app.js
@@ -1,8 +1,14 @@
+// This React component serves as the entry point for your Next.js application.
+// It includes the necessary global styles from "@/styles/globals.css" and utilizes
+// the SessionProvider from next-auth/react to manage user authentication sessions.
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 
+// The main App component receives Component and pageProps as props, where Component
+// represents the current page component and pageProps holds the initial props for that component.
 export default function App({ Component, pageProps }) {
   return (
+    // Wrapping the entire application with SessionProvider to enable authentication session management.
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
     </SessionProvider>

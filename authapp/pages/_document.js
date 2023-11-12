9eba64ced4ccaa13585935337d7f2e6efba4aa74
leo77534 <68187502+leo77534@users.noreply.github.com>
2023-11-12 20:27:33 +1100

    Docs: add comments

--- authapp/pages/_document.js
+++ authapp/pages/_document.js
@@ -1,3 +1,18 @@
+/**
+ * This custom document file is used to modify the overall structure
+ * of the HTML document rendered by Next.js. It extends the default
+ * NextDocument component and includes customizations for the <Html>,
+ * <Head>, <body>, <Main>, and <NextScript> components.
+ *
+ * The <Html> component sets the language attribute to 'en'.
+ * The <Head> component can be used to add custom metadata.
+ * The <body> component includes the <Main> component, which
+ * renders the main content of the page, and the <NextScript>
+ * component, which includes the necessary scripts for Next.js
+ * functionality.
+ *
+ * @see https://nextjs.org/docs/advanced-features/custom-document
+ */
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {

801eb1190f45c5f87dae2af1670b2cecd7530f15
leo77534 <68187502+leo77534@users.noreply.github.com>
2023-11-12 20:16:43 +1100

    Docs: add comments

--- authapp/layout/layout.js
+++ authapp/layout/layout.js
@@ -1,3 +1,22 @@
+/**
+ * Layout Component:
+ *
+ * This component serves as a basic layout structure for the application,
+ * providing a centered container with a specified width and height.
+ *
+ * Props:
+ * - children: The content to be placed in the center of the layout.
+ *
+ * The outer container uses flex to center its content vertically on the screen,
+ * with a background color of 'bg-blue-400'.
+ *
+ * The inner container is centered both horizontally and vertically using margin auto,
+ * and it has a background color of 'bg-slate-50', rounded corners, and a specified width
+ * (3/5 of the parent) and height (3/4 of the parent).
+ *
+ * Inside the inner container, the children are placed in the center of the container
+ * with 'text-center' class.
+ */
 export default function Layout({ children }) {
   return (
     <div className="flex h-screen bg-blue-400">

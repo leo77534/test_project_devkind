32c26abd0b17da246775fd0d615023d92eb8a64e
leo77534 <68187502+leo77534@users.noreply.github.com>
2023-11-12 20:23:12 +1100

    Docs: add comments

--- authapp/pages/api/auth/signup.js
+++ authapp/pages/api/auth/signup.js
@@ -2,25 +2,31 @@ import connectMongo from "../../../database/conn";
 import Users from "../../../model/Schema";
 import { hash } from "bcryptjs";
 
+// Async function to handle user registration
 export default async function handler(req, res) {
+  // Connect to MongoDB, handle connection failure
   connectMongo().catch((error) => res.json({ error: "Connection Failed...!" }));
 
-  // only post method is accepted
+  // Only accept POST requests for user registration
   if (req.method === "POST") {
+    // Return an error if no form data is provided
     if (!req.body)
       return res.status(404).json({ error: "Don't have form data...!" });
+    // Extract username, email, and password from request body
     const { username, email, password } = req.body;
     console.log(req.body);
 
-    // check duplicate users
+    // Check for duplicate users with the same email
     const checkexisting = await Users.findOne({ email });
     if (checkexisting)
       return res.status(422).json({ message: "User Already Exists...!" });
 
-    // hash password
+    // Hash the password and create a new user in the database
     Users.create({ username, email, password: await hash(password, 12) });
+    // Respond with a success message and user information
     res.status(201).json({ status: true, user: { username, email, password } });
   } else {
+    // Return an error if the HTTP method is not valid (only POST accepted)
     res
       .status(500)
       .json({ message: "HTTP method not valid only POST Accepted" });

6a353327f51aa7438f88e563bd4f8230dcc6ecdf
leo77534 <68187502+leo77534@users.noreply.github.com>
2023-11-12 20:19:13 +1100

    Docs: add comments

--- authapp/model/Schema.js
+++ authapp/model/Schema.js
@@ -1,11 +1,14 @@
 import { Schema, model, models } from "mongoose";
 
+// Defining the user schema with username, email, and password fields
 const userSchema = new Schema({
   username: String,
   email: String,
   password: String,
 });
 
+// Creating a Mongoose model for the "user" collection, or using an existing one if available
 const Users = models.user || model("user", userSchema);
 
+// Exporting the Users model for use in other parts of the application
 export default Users;

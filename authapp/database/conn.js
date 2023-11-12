0305dcccd35c4f345c53d334d852ac355e6e9f99
leo77534 <68187502+leo77534@users.noreply.github.com>
2023-11-12 20:15:56 +1100

    Docs: add comments

--- authapp/database/conn.js
+++ authapp/database/conn.js
@@ -1,17 +1,18 @@
 import mongoose from "mongoose";
 
+// Async function to connect to MongoDB using the provided MONGO_URL from the environment variables.
 const connecMongo = async () => {
   try {
     const { connection } = await mongoose.connect(process.env.MONGO_URL);
 
     if (connection.readyState == 1) {
       console.log("Database Connected!");
-      return Promise.resolve(true);
+      return Promise.resolve(true); // Resolving the promise with a success flag.
     }
   } catch (error) {
     console.log(error);
-    return Promise.reject(error);
+    return Promise.reject(error); // Rejecting the promise with the encountered error.
   }
 };
-
+// Exporting the connectMongo function to make it available for use in other modules.
 export default connecMongo;
